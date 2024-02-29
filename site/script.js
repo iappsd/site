5ca82b1f818fd8ecb3ddd6125baf1d452fad857f
iappsd <alhurais@ju.edu.sa>
2024-02-29 20:31:32 +0300

    up

--- site/script.js
+++ site/script.js
@@ -1,1 +1,8 @@
-console.log("Choo choo!");
+function calculateGPA() {
+    var firstYear = document.getElementById('firstYear').value;
+    var secondYear = document.getElementById('secondYear').value;
+    var thirdYear = document.getElementById('thirdYear').value;
+
+    var gpa = (firstYear * 0.2) + (secondYear * 0.4) + (thirdYear * 0.4);
+    document.getElementById('result').innerHTML = `المعدل التراكمي هو: ${gpa}`;
+}
